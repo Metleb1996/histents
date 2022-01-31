793322dd2cf2e5c2484797aeb901a196a5787cec
Metleb1996 <metlebrustemov1996@gmail.com>
2022-01-31 17:54:00 +0400

    Added models

---
+++ core/models/__init__.py
@@ -0,0 +1,36 @@
+__all__ = ['app', 'db', 'User', 'Event']
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from core.constants import DB_NAME
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(os.path.join(os.path.dirname(__file__), '{}'.format(DB_NAME)))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+    role = db.Column(db.Integer, default=0, nullable=False)
+    verified = db.Column(db.Boolean, default=False, nullable=False)
+    events = db.relationship("Event", backref='user')
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    e_text = db.Column(db.String(2048), unique=True, nullable=False)
+    e_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+
+    def __repr__(self):
+        return '<Event %r>' % self.id
