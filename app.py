a5b4e17a9fd6ae799d7b78a1fbf42d8ffc6362d3
Metleb1996 <metlebrustemov1996@gmail.com>
2022-01-31 17:42:15 +0400

    --

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask
 from core import constants
 from core.api.v1 import apiv1 as api_v1
+from core.models import app
 
-app = Flask(__name__)
 app.register_blueprint(api_v1, url_prefix='/api/v1')
 
 @app.route("/")
