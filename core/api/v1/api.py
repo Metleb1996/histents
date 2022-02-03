afb686d17192c0b504a3456222c713f47dfbfbad
Metleb1996 <metlebrustemov1996@gmail.com>
2022-02-03 21:37:46 +0400

    Event class

--- core/api/v1/api.py
+++ core/api/v1/api.py
@@ -1,8 +1,8 @@
 import datetime
 from flask import Blueprint, jsonify
 from flask_restful import Resource, Api, reqparse
-from core.models import db, User
-from core.helper import is_email, user_data_control
+from core.models import db, User, Event
+from core.helper import is_email, user_data_control, event_data_control
 
 bp_api = Blueprint('apiv1', __name__ )
 api = Api(bp_api)
@@ -78,16 +78,88 @@ class HEventResource(Resource):
         self.parser.add_argument("user_password", type=str, required=True, location=("json", "values"))
         self.parser.add_argument("e_text", type=str, required=True, location=("json", "values"))
         self.parser.add_argument("e_date", type=datetime.datetime, required=True, location=("json", "values"))
-        self.parser.add_argument("new_e_text", type=str, required=True, location=("json", "values"))
-        self.parser.add_argument("new_e_date", type=datetime.datetime, required=True, location=("json", "values"))
+        self.parser.add_argument("e_id", type=int, required=False, location=("json", "values"))
+        self.parser.add_argument("new_e_text", type=str, required=False, location=("json", "values"))
+        self.parser.add_argument("new_e_date", type=datetime.datetime, required=False, location=("json", "values"))
     def get(self): 
-        return {'hello':'world'}
+        data = self.parser.parse_args()
+        data, ok = user_data_control(data)
+        if not ok:
+            return jsonify(message=data)
+        data = self.parser.parse_args()
+        _data, ok = event_data_control(data, id_ccontrol=True)
+        if ok:
+            e = Event.query.filter_by(id=_data['e_id']).first()
+            if e is not None:
+                return jsonify(e_date=e.e_date, e_text=e.e_text, message='OK')
+            return jsonify(message="Event information is incorrect.")
+        else:
+            data, ok = event_data_control(data)
+            if not ok:
+                return jsonify(message=data)
+            e = Event.query.filter_by(e_date=_data['e_date']).all()
+            if len(e)>0:
+                return jsonify(e_date=data['e_date'], e_text=[ev['e_text'] for ev in e], message='OK')
+
     def post(self):
-        return {'hello':'world'}
+        data = self.parser.parse_args()
+        data, ok = user_data_control(data)
+        if not ok:
+            return jsonify(message=data)
+        data, ok = event_data_control(data)
+        if not ok:
+            return jsonify(message=data)
+        try:
+            u = User.query.filter_by(username=data['user_name'], email=data['user_email'], password=data['user_password']).first()
+            if u is not None:
+                e = Event(e_text=data['e_text'], e_date=data['e_date'], user=u)
+                db.session.add(e)
+                db.session.commit()
+            else:
+                return jsonify(message="User or event information is incorrect.")
+        except Exception as e:
+            return jsonify(message=str(e))
     def put(self):
-        return {'hello':'world'}
+        data = self.parser.parse_args()
+        data, ok = user_data_control(data)
+        if not ok:
+            return jsonify(message=data)
+        data, ok = event_data_control(data, id_ccontrol=True)
+        if not ok:
+            return jsonify(message=data)
+        try:
+            new_data, ok = event_data_control({'e_text':data['new_e_text'], 'e_date':data['new_e_date']})
+            if not ok:
+                return jsonify(message=new_data)
+            u = User.query.filter_by(username=data['user_name'], email=data['user_email'], password=data['user_password']).first()
+            e = Event.query.filter_by(id=data['e_id']).first()
+            if u is not None and e is not None and e.user == u:
+                e.e_text = data['new_e_text']
+                e.e_date = data['new_e_date'] 
+                db.session.commit()
+            else:
+                return jsonify(message="User or event information is incorrect.")
+        except Exception as e:
+            return jsonify(message=str(e))
+
     def delete(self):
-        return {'hello':'world'}
+        data = self.parser.parse_args()
+        data, ok = user_data_control(data)
+        if not ok:
+            return jsonify(message=data)
+        data, ok = event_data_control(data, id_ccontrol=True)
+        if not ok:
+            return jsonify(message=data)
+        try:
+            u = User.query.filter_by(username=data['user_name'], email=data['user_email'], password=data['user_password']).first()
+            e = Event.query.filter_by(id=data['e_id']).first()
+            if u is not None and e is not None and e.user == u:
+                db.session.delete(e) 
+                db.session.commit()
+            else:
+                return jsonify(message="User or event information is incorrect.")
+        except Exception as e:
+            return jsonify(message=str(e))
 
 api.add_resource(UserResource, '/user')
 api.add_resource(HEventResource, '/event')
